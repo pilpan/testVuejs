0280afb2417bf7983b44db7463e86830b9e2b6c0
plip <plip2010195@yandex.ru>
2023-10-16 11:01:32 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <p>Hello world</p>
+  <div class="container"></div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style>
+  .container{
+    background-color: red;
+    width: 40px;
+    height: 40px;
+  }
+</style>
